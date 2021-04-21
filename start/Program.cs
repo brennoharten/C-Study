b7c40297d0db81ec27ac21a1aaf4266c7af47cc1
brennoharten <brennoharten@gmail.com>
2021-04-21 20:54:13 -0300

    new project web whith c#

--- start/Program.cs
+++ start/Program.cs
@@ -4,52 +4,16 @@ namespace start
 {
     class Program
     {
+        enum DiasSemana{domingo, segunda, terça, quarta, quinta, sexta, sabado};
         static void Main(String[] args) {
 
-            int opcao;
+            int num = 10;
+	        Console.Write(dobrar(num));
 
-            Console.WriteLine("Selecione uma opção ");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("1 - para desbloqueio");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("2 - para bloqueio   ");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("3 - para cédito     ");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("4 - para sair       ");
-            Console.WriteLine("--------------------");
+        }
 
-            opcao = int.Parse(Console.ReadLine());
-
-            switch(opcao)  {
-                case 1: 
-                {
-                    Console.WriteLine("cartao desbloqueado");
-                    break;
-                } 
-                case 2: 
-                {
-                    Console.WriteLine("cartao bloqueado");
-                    break;
-                }
-                case 3: 
-                {
-                    Console.WriteLine("credito solicitado");
-                    break;
-                }
-                case 4: 
-                {
-                    Console.Clear();
-                    Console.WriteLine("Volte sempre");
-                    break;
-                }
-                default: 
-                {
-                    Console.WriteLine("Opção não existe");
-                    break;
-                }
-
-            }
+        static int dobrar(int valor) {
+            return valor *= 2;
         }
     }
 }
