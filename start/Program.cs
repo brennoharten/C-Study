13c9fc59f01b2785665b1561af1bc31b917c8b9c
brennoharten <brennoharten@gmail.com>
2021-04-27 18:58:57 -0300

    remove some trash

--- start/Program.cs
+++ start/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace start
 {
     class Program
     {
-        enum DiasSemana{domingo, segunda, terça, quarta, quinta, sexta, sabado};
         static void Main(String[] args) {
+           
+            Queue<string> veiculos = new Queue<string>();
 
-            int num = 10;
-	        Console.Write(dobrar(num));
+            veiculos.Enqueue("Carro");
+            veiculos.Enqueue("mooto");
+            veiculos.Enqueue("aviao");
+            veiculos.Enqueue("bike");
+            veiculos.Enqueue("skate");
 
-        }
+            string primeiro = veiculos.Dequeue();
+            string segundo = veiculos.Peek();
 
-        static int dobrar(int valor) {
-            return valor *= 2;
+            foreach(string v in veiculos) {
+                Console.WriteLine(v);
+            }
+
+            // nao se pode aplicar indexação na quee
+        
+            
         }
     }
 }
