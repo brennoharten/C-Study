383ef3c084e55bc4fadcda671aa0b9df21755daa
brennoharten <brennoharten@gmail.com>
2021-11-15 08:22:48 -0300

    atualizando repositorio

---
+++ ByteBankConta/ContaCorrente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ByteBankConta
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public static double TaxaOperacao { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+        
+        public int Numero { get; }
+        public int Agencia{ get; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if(agencia <= 0) 
+            {
+                throw new ArgumentException("A agencia deve ser maiores qeu zero", nameof(agencia));
+
+            }
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O numero deve ser maiores qeu zero", nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 /TotalDeContasCriadas;
+        }
+
+
+        public void Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                throw new SaldoInsuficienteException();
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ ByteBankConta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ByteBankConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(456, 4446457);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(500);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("o parametro " + e.ParamName + "nao pode ser igual a zero");
+            }
+            catch(SaldoInsuficienteException e)
+            {
+                Console.WriteLine("Exceção do tipo saldo insuficiente");
+            }
+        }
+    }
+}
