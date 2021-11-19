cfe7d3b0ecd0d7359d0719da48f023ef812db057
brennoharten <brennoharten@gmail.com>
2021-11-19 06:27:42 -0300

    criando novo projeto interno

---
+++ ByteBank.Interno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458,455755);
+        }
+    }
+}
